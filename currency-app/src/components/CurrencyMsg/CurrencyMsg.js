699503267f12c287cc778f9cb18dba4009c3e980
Rafli Bangsawan <rafli.bangsawan@ui.ac.id>
2022-09-13 15:45:11 +0700

    modified CSS

--- currency-app/src/components/CurrencyMsg/CurrencyMsg.js
+++ currency-app/src/components/CurrencyMsg/CurrencyMsg.js
@@ -1,7 +1,7 @@
 export const CurrencyMsg = () => {
     return (
         <div className="currency__message">
-            <p>Rates are based from 1 USD</p>
+            <p>Rates are based from 1 USD.</p>
             <p>This application uses API from https://currencyfreaks.com.</p>
         </div>
     );
